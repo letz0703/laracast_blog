6b094feccd0f04cd7ff3aefc8640c0ce9658dfde
letz0703 <letz0703@gmail.com>
2018-03-10 10:01:51 +0900

    Routing Model Binding

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Route::get('/tasks','TasksController@index');
 
-Route::get('/tasks/{id}', 'TasksController@show');
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/about', function () {
     return view('/about');
