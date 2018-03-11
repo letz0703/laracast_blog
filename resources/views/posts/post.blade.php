c62fd4e873eafb26a61826eb401031dc383436c5
letz0703 <letz0703@gmail.com>
2018-03-11 23:07:07 +0900

    render posts

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,16 +1,12 @@
-<main role="main" class="container">
-      <div class="row">
-        <div class="col-md-8 blog-main">
+          <div class="blog-post">
+            <h2 class="blog-post-title">{{ $post->title}}</h2>
+            <p class="blog-post-meta">{{ $post->created_at->toDayDateTimeString() }}</p>
 
-      <div class="blog-post">
-            <h2 class="blog-post-title">{{ $post->title }}</h2>
-            <p class="blog-post-meta">{{ $post->created_at }}</p>
-		        {{ $post->body}}
-		  </div>
-
+            <p>
+              {{ $post->body}}
+            </p>
+          </div><!-- /.blog-post -->
 
 
-        </div><!-- /.blog-main -->
-</div>
-@include('layouts.page-nav')
-</main>
+
+
