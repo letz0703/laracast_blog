f934fa04f92128a8c410022e8ec3f2dc903e3a52
letz0703 <letz0703@gmail.com>
2018-03-12 11:39:28 +0900

    show a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,11 @@ class PostController extends Controller
     	return view('posts.index',compact('posts'));
     }    //
 
-    public function show()
+    public function show(Post $post)
     {
-    	return view('posts.show');
-    }    //
+        // $post = Post::find($id);
+    	return view('posts.show',compact('post'));
+    }
 
     public function create()
     {
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
   <div class="row">
     <div class="col-md-8 blog-main">
       <h3 class="pb-3 mb-4 font-italic border-bottom">
-        Posts
+        Letz Blog
       </h3>
       @foreach ($posts as $post)
         @include('posts/post')
